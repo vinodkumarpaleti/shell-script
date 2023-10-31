e7ce35524402339640745d49973dde3267ad57f1
vinodkumar <kumarvinod.paleti@gmail.com>
2023-10-31 16:19:38 +0530

    array file added

--- 06-array.sh
+++ 06-array.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 NUMBERS=("54" "95" "100") #This is array it has collection of elements
-echo "The numbers are ${NUMBERS[2]}" # It get the value based on the index number
+echo "The 2nd index number is ${NUMBERS[2]}" # It get the value based on the index number
 
 PERSONS=("Vinod" "Kumar" "Paleti")
 echo "Surname is : ${PERSONS[2]}"
 
-echo "All Numbers are : ${[@]}"
-echo "All persons are : ${[@]}"
+echo "All Numbers are : ${NUMBERS[@]}"
+echo "All persons are : ${PERSONS[@]}"
