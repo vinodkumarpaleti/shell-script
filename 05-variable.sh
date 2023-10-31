1ac060b56541e53b719127b803dedc7aa8bd7ec9
vinodkumar <kumarvinod.paleti@gmail.com>
2023-10-31 15:55:23 +0530

    read the input

--- 05-variable.sh
+++ 05-variable.sh
@@ -4,5 +4,5 @@ echo "Enter your username"
 read USERNAME
 echo "Enter your Password"
 read -s PASSWORD
-echo "You entered username $USERNAME"
-echo "You entered password $PASSWORD" # We dont expose the passwords, this is for prctice purpose
+echo "You entered username : $USERNAME"
+echo "You entered password : $PASSWORD" # We dont expose the passwords, this is for prctice purpose
