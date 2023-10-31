2ef610f95262f744c45132e657810c719bbc1dd6
vinodkumar <kumarvinod.paleti@gmail.com>
2023-10-31 15:53:44 +0530

    Addition of two numbers

---
+++ 05-variable.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+echo "Enter your username"
+read USERNAME
+echo "Enter your Password"
+read -s PASSWORD
+echo "You entered username $USERNAME"
+echo "You entered password $PASSWORD" # We dont expose the passwords, this is for prctice purpose
