f2b548359ea0343014d163e5ae97fb5dc3c1007a
vinodkumar <kumarvinod.paleti@gmail.com>
2023-10-31 15:34:32 +0530

    New member added

--- 03-variable.sh
+++ 03-variable.sh
@@ -4,6 +4,7 @@ echo "Hello World, I am learning DevOps with AWS"
 PERSON1=$1 # this is the first value from command line
 PERSON2=$2 # # this is the second value from command line
 PERSON3=$3 # this is the third value from command line
+#Here we pass the arguments in the command line
 
 echo "$PERSON1: Hi $PERSON2, Good Morning"
 echo "$PERSON2: Good Morning $PERSON1"
