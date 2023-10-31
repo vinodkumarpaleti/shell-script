2696ee7abfbfaae3532f8777b6f1b5e69c499d84
vinodkumar <kumarvinod.paleti@gmail.com>
2023-10-31 17:48:38 +0530

    install mysql script file added

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -6,10 +6,22 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-echo "ERROR: Run this script with root access"
-exit 1
+    echo "ERROR: Run this script with root access"
+    exit 1
 fi
 
 
 # It is our resposibility to check weather the installation is succes or not
-#yum install mysql -y
+#If "echo $?" checks weather the command is success or not
+#If 0 is there the command is success
+#If 1-127 number are there it is not success
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR: mysql installation is faild"
+    exit 1
+else
+    echo "mysql installation is success...!"
+fi
