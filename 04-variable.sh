69f560e6c51f81363a3bb999a1aa3f185a28ee77
vinodkumar <kumarvinod.paleti@gmail.com>
2023-10-31 15:42:22 +0530

    Addition of two numbers

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+#Addition of two numbers
+NUMBER1=$1  #It Will dynamically read the input from the command line
+NUMBER2=$2  #It Will dynamically read the input from the command line
+
+SUM=$(NUMBER1+NUMBER2)
+
+echo "Addtion of two numbers is: $SUM"
