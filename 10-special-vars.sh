7f3883e31e68675c01ce71871e33495d854e7a93
vinodkumar <kumarvinod.paleti@gmail.com>
2023-11-01 17:35:49 +0530

    special vars file is changes

--- 10-special-vars.sh
+++ 10-special-vars.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-VAR1=$1
-VAR2=$2
-echo "script name: $0"  #It prints the script name of the file
-echo "variable 1: $VAR1"
+VAR1=$1  # Here it will take the input from the command line
+VAR2=$2  # Here it will take the input from the command line
 
+echo "script name: $0"   #It prints the script name of the file
+echo "variable 1: $VAR1" 
+echo "varible 2: $VAR2"
 echo "all vars: $@"  #It prints all variables that we defined
 
 echo "number of vars: $#" #It prints the total number of variables
