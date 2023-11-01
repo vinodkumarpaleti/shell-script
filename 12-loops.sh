e7a463c92214382e67f31f3d8e330b81fec35465
vinodkumar <kumarvinod.paleti@gmail.com>
2023-11-01 21:28:11 +0530

    loops file is changed

---
+++ 12-loops.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+
+USER=$(id -u)  # It checks the user
+R="\e[31m" # It gives the colour red
+N="\e[0m" # It gives the color normal
+
+if [ $USER -ne 0 ]
+then
+    echo "$R Error: Please run this script with root access $N"
+    exit 1
+fi
+
+
+for i in $@
+do
+    echo yum install $i
+done
