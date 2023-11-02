708cfa2ebc930d7749fbe2c964f7a241a76f46b7
vinodkumar <kumarvinod.paleti@gmail.com>
2023-11-02 22:13:49 +0530

    disk usage file added

--- 14-disk-usage.sh
+++ 14-disk-usage.sh
@@ -37,5 +37,5 @@ echo -e "message: $message"
 #echo "$message" | mail -s "High Disk usage" info@joindevops.com
 
 #how to call other shell script from your current script
-sh mail.sh kumarvinod@joindevops.com "High Disk Usage" "$message" "DEVOPS TEAM" "High Disk usage"
+sh mail.sh vinodpaleti1@gmail.com "High Disk Usage" "$message" "DEVOPS TEAM" "High Disk usage"
 
